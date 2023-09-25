189ea8ffdbd65f107811e100974528ad575bdbb1
RemiSaurel <r.saurel5@gmail.com>
2023-09-25 19:33:52 +0200

    fix: circular imports

---
+++ functions.py
@@ -0,0 +1,71 @@
+import time
+import urllib
+from pathlib import Path
+
+import requests
+API_URL = "https://wasabi.i3s.unice.fr"
+
+def fetch_artist(artist_name):
+    """Fetch artist data from the API."""
+    # Encode the artist name to avoid errors, e.g. "AC/DC" -> "AC%2FDC"
+    artist_encoded = urllib.parse.quote(artist_name)
+    print("Encoded : " + artist_encoded)
+    response = requests.get(API_URL + "/api/v1/artist_all/name/" + artist_encoded)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error: Unable to fetch artist data. Status code: {response.status_code}")
+        return None
+
+
+# GET A FIELD FROM AN ARTIST DATA
+def get_field(artist_data, field):
+    if artist_data is None:
+        return []
+
+    if field not in artist_data:
+        print("No " + field + " found for " + artist_data["name"])
+        return []
+
+    print("Found " + str(len(artist_data[field])) + " " + field + " for " + artist_data["name"])
+    return artist_data[field]
+
+
+def request_artists(ARTISTS):
+    # Dictionary to keep track of fetched artist data
+    fetched_artists = {}
+
+    # Write artist json data into a json file for each artist
+    for artist in ARTISTS:
+        artist_json_path = Path("data") / f"{artist}.json"
+
+        # Check if artist data has already been fetched
+        if artist_json_path.is_file():
+            if artist not in fetched_artists:
+                # Check if the file contains "None", if yes, fetch the data again
+                with open(artist_json_path, "r", encoding="utf-8") as f:
+                    artist_data = eval(f.read())
+                    if artist_data is None:
+                        time.sleep(3)
+                        print("Fetching data for " + artist)
+                        artist_data = fetch_artist(artist)
+                        f.write(str(artist_data))
+                    else:
+                        print("Using cached data for " + artist)
+                        fetched_artists[artist] = artist_data
+        else:
+            time.sleep(3)
+            print("Fetching data for " + artist)
+            artist_data = fetch_artist(artist)
+            with open(artist_json_path, "w", encoding="utf-8") as f:
+                f.write(str(artist_data))
+
+
+def get_data_from_file(file):
+    with open(file, "r", encoding="utf-8") as f:
+        artist_data = eval(f.read())
+        if artist_data is not None:
+            return artist_data
+        else:
+            return None
--- main.py
+++ main.py
@@ -1,80 +1,6 @@
-import time
-
-import requests as requests
-import urllib.parse
-
 # IMPORT YOUR FILE HERE
+from functions import request_artists
 from remi import *
-
-API_URL = "https://wasabi.i3s.unice.fr"
-
-
-# FETCH ARTIST DATA FROM THE API
-def fetch_artist(artist_name):
-    """Fetch artist data from the API."""
-    # Encode the artist name to avoid errors, e.g. "AC/DC" -> "AC%2FDC"
-    artist_encoded = urllib.parse.quote(artist_name)
-    print("Encoded : " + artist_encoded)
-    response = requests.get(API_URL + "/api/v1/artist_all/name/" + artist_encoded)
-
-    if response.status_code == 200:
-        return response.json()
-    else:
-        print(f"Error: Unable to fetch artist data. Status code: {response.status_code}")
-        return None
-
-
-# GET A FIELD FROM AN ARTIST DATA
-def get_field(artist_data, field):
-    if artist_data is None:
-        return []
-
-    if field not in artist_data:
-        print("No " + field + " found for " + artist_data["name"])
-        return []
-
-    print("Found " + str(len(artist_data[field])) + " " + field + " for " + artist_data["name"])
-    return artist_data[field]
-
-
-def request_artists():
-    # Dictionary to keep track of fetched artist data
-    fetched_artists = {}
-
-    # Write artist json data into a json file for each artist
-    for artist in ARTISTS:
-        artist_json_path = Path("data") / f"{artist}.json"
-
-        # Check if artist data has already been fetched
-        if artist_json_path.is_file():
-            if artist not in fetched_artists:
-                # Check if the file contains "None", if yes, fetch the data again
-                with open(artist_json_path, "r", encoding="utf-8") as f:
-                    artist_data = eval(f.read())
-                    if artist_data is None:
-                        time.sleep(3)
-                        print("Fetching data for " + artist)
-                        artist_data = fetch_artist(artist)
-                        f.write(str(artist_data))
-                    else:
-                        print("Using cached data for " + artist)
-                        fetched_artists[artist] = artist_data
-        else:
-            time.sleep(3)
-            print("Fetching data for " + artist)
-            artist_data = fetch_artist(artist)
-            with open(artist_json_path, "w", encoding="utf-8") as f:
-                f.write(str(artist_data))
-
-
-def get_data_from_file(file):
-    with open(file, "r", encoding="utf-8") as f:
-        artist_data = eval(f.read())
-        if artist_data is not None:
-            return artist_data
-        else:
-            return None
-
 
 if __name__ == "__main__":
     # LIST OF ARTISTS
@@ -86,9 +12,11 @@ if __name__ == "__main__":
 
     # WARNING :
     # TURN ON TO GET NEW ARTISTS OR RELOAD SOME
-    # request_artists()
+    # request_artists(ARTISTS)
 
     # REMI ANALYSIS
     main_analysis(ARTISTS)
 
+    # CHAIMAE ANALYSIS
+    main_analysis2(ARTISTS)
     # PUT YOUR ANALYSIS HERE
