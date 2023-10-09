661b3809bbab83194dba871858ef8fd5fafd6173
lempichaimae <chaimae.khamma@gmail.com>
2023-10-09 09:47:01 +0200

    add: avg_fans_albums

--- chaimae.py
+++ chaimae.py
@@ -1,10 +1,11 @@
 import datetime
-
+import json
 from functions import get_data_from_file, get_field
 
 
 def get_life_span(lifespan_by_artist):
-    artist_career ={}
+    artist_career = {}
+
     with open("analysis/lifespan.json", "a", encoding="utf-8") as f:
         f.truncate(0)
         f.write("[\n")
@@ -18,34 +19,53 @@ def get_life_span(lifespan_by_artist):
                 duration = int(end) - int(begin)
             else:
                 duration = 0
-            f.write('{"artist": "' + artist + '", "begin": "' + str(begin) +'", "end": "'+ str(end) +'", "duration": "'+str(duration)+ '"},\n')
+            f.write('{"artist": "' + artist + '", "begin": "' + str(begin) + '", "end": "' + str(
+                end) + '", "duration": "' + str(duration) + '"},\n')
         f.write("]\n")
+
+
 def generate_artists_infos(artist_list, albums_by_artist, deezer_fans):
     artist_infos = {}
+    avg_likes_by_artist = {}
     with open("analysis/artists_infos.json", "a", encoding="utf-8") as f:
         f.truncate(0)
         f.write("[\n")
         for artist in artist_list:
             nb_songs = 0
+            sum_fans = 0
             nb_albums = len(albums_by_artist[artist])
             for album in albums_by_artist[artist]:
                 nb_songs += len(album["songs"])
-            artist_infos[artist] = { "nbAlbums": nb_albums, "nbSongs": nb_songs,
-                                    "deezerFans": deezer_fans[artist]}
-            f.write('{"artist": "' + artist + ', "nbAlbums": ' + str(
-                nb_albums) + ', "nbSongs": ' + str(nb_songs) + ', "deezerFans": ' + str(deezer_fans[artist]) + "},\n")
+                count_albums = nb_albums
+                if "deezerFans" not in album:
+                    likes = 0
+                    count_albums = count_albums-1
+                else:
+                    likes = album["deezerFans"]
+                sum_fans += int(likes)
+            avg_albums_fans = sum_fans/count_albums
+            artist_infos[artist] = {"nbAlbums": nb_albums, "nbSongs": nb_songs,
+                                    "deezerFans": deezer_fans[artist], "avgAlbumsFans": avg_albums_fans}
+            f.write('{"artist": "' + artist + '", "nbAlbums": ' + str(
+                nb_albums) + ', "nbSongs": ' + str(nb_songs) + ', "deezerFans": ' + str(deezer_fans[artist]) +
+                    ', "avgAlbumsFans": ' + str(avg_albums_fans) + "},\n")
         f.write("]\n")
+
     return artist_infos
+
+
 def main_data(artist_list):
     # READ DATA FROM FILES
     lifespan_by_artist = {}
     albums_by_artist = {}
     deezer_fans_by_artist = {}
-    artist_infos = {}
+
     for artist in artist_list:
         artist_data = get_data_from_file("data/" + artist + ".json")
         lifespan_by_artist[artist] = get_field(artist_data, "lifeSpan")
-        albums_by_artist[artist] =get_field(artist_data, "albums")
+        albums_by_artist[artist] = get_field(artist_data, "albums")
         deezer_fans_by_artist[artist] = get_field(artist_data, "deezerFans")
-    generate_artists_infos(artist_list,  albums_by_artist, deezer_fans_by_artist)
+    generate_artists_infos(artist_list, albums_by_artist, deezer_fans_by_artist)
     get_life_span(lifespan_by_artist)
+# json_object = json.dumps(albums_by_artist, indent=4)
+# print(json_object)
