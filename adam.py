631b906198b1d1229cdb15e5bde88c7375cbcea1
AdamKhalepo <adam.khalepo@protonmail.com>
2023-10-09 18:10:12 +0200

    added: added values for artists in genre

--- adam.py
+++ adam.py
@@ -1,33 +1,7 @@
-import json
-
 import requests
 import time
 
 from functions import API_URL
-
-
-def artist_popularity_by_genre(data):
-    """Get the popularity of each artist and divide it by the number of genres they have
-    :artists dict: The response from the API
-    """
-    genres = []
-    artists = []
-    for artist in data:
-        artist_global_popularity = artist["deezerFans"] if "deezerFans" in artist else 0
-        artist_genres = artist["genres"]
-        if len(artist_genres) == 0:
-            artist_genres = ["Unknown"]
-        artists_infos = {"name": artist["name"], "popularity": artist_global_popularity, "genres": artist_genres}
-        artists.append(artists_infos)
-        for genre in artist_genres:
-            if genre not in genres:
-                genres.append(genre)
-        # print(artist["name"] + " = " + str(popularity_by_genre) + " fans & " + str(
-        #     len(artist_genres)) + " genres " + str(artist_genres))
-    # print("Total genres : " + str(len(genres)))
-    # print(genres)
-    # print(artists)
-    write_json_genres(artists, genres)
 
 
 def fetch_artists(START=0):
@@ -50,27 +24,69 @@ def fetch_artists(START=0):
         return None
 
 
-# regroup the genres by a global genre (alternative rock, ska rock, punk rock -> rock)
-def regroup_genres(genres):
-    return ""
+def artist_popularity_by_genre(data):
+    """Get the popularity of each artist and divide it by the number of genres they have
+    :data dict: The response from the API
+    """
+    genres = []
+    artists = []
+    for artist in data:
+        artist_global_popularity = artist["deezerFans"] if "deezerFans" in artist else 0
+        artist_genres = artist["genres"]
+        nb_albums = len(artist["albums"])
+        artist_country = artist["location"]["country"]
+        nb_songs = 0
+        for album in artist["albums"]:
+            nb_songs += len(album["songs"])
+
+        if len(artist_genres) == 0:
+            artist_genres = ["Unknown"]
+        artists_infos = {
+            "name": artist["name"],
+            "popularity": artist_global_popularity,
+            "genres": artist_genres,
+            "nbAlbums": nb_albums,
+            "nbSongs": nb_songs,
+            "country": artist_country
+        }
+        artists.append(artists_infos)
+        for genre in artist_genres:
+            if genre not in genres:
+                genres.append(genre)
+    write_json_genres(artists, genres)
 
 
 def write_json_genres(artists, genres):
+    """Write the artists and genres data into a json file
+    :param list artists: The list of artists
+    :param list genres: The list of genres
+    """
+
     with open("analysis/adam_genre.json", "a", encoding="utf-8") as f:
         f.truncate(0)
-        f.write("[\n")
-        print(artists)
-        print(genres)
+        f.write("[")
         for genre in genres:
-            f.write('{"genre": "' + str(genre) + '",\n')
-            f.write(' "artists": [\n')
+            f.write('{"genre": "' + str(genre) + '",')
+            f.write(' "artists": [')
             for artist in artists:
                 if genre in artist["genres"]:
                     artist_name = artist["name"]
+                    nb_albums = artist["nbAlbums"]
+                    nb_songs = artist["nbSongs"]
+                    country = artist["country"]
                     popularity_by_genre = round(artist["popularity"] / len(artist["genres"]))
-                    f.write('  {"name": "' + artist_name + '", "nbFans": ' + str(popularity_by_genre) + '},\n')
-            f.write(' ]\n')
-            f.write("},\n")
-        f.write("]\n")
+                    f.write('  {"name": "' + artist_name + '",'
+                            + '"nbFans": ' + str(popularity_by_genre) + ','
+                            + ' "nbFans": ' + str(popularity_by_genre) + ','
+                            + '"nbAlbums": ' + str(nb_albums) + ','
+                            + '"nbSongs": ' + str(nb_songs) + ','
+                            + '"country": ' + str(country) + '},')
+            f.write(' ]')
+            f.write("},")
+        f.write("]")
 
 
+#TODO
+# regroup the genres by a global genre (alternative rock, ska rock, punk rock -> rock)
+def regroup_genres(genres):
+    return ""
